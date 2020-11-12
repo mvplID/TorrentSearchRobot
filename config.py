4fd36213b8c6ae10992e83b0338e06c5ea140105
Ayush Sharma <aayush240204@gmail.com>
2020-11-12 17:50:30 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+TOKEN = os.environ.get("BOT_TOKEN")
+START_MESSAGE = os.environ.get("START_MESSAGE", "*Hello!!\nI Am Torrent Searcher Robot Helps You To Find Fast Magnet Links\n\n\nMade with ❤️ By @CyberBoyAyush*")
+FOOTER_TEXT = os.environ.get("FOOTER_TEXT", "*Made with ❤️ by @CyberBoyAyush*")
